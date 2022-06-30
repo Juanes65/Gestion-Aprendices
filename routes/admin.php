da4b31c258cc32d7db656bc092d3cd30e35ca15a
Juanes <juanflorezospina22@gmail.com>
2022-06-30 11:29:14 -0500

    2

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/fichas',             [FichaController::class, 'index']) ->name('index.ficha');
 Route::get('/fichas/agregar',     [FichaController::class, 'create'])->name('create.ficha');
 Route::post('/fichas/almacenar',  [FichaController::class, 'store']) ->name('store.ficha');
-Route::get('/ficha/editar/{ficha}', [FichaController::class, 'edit'])->name('edit.ficha');
-Route::put('/ficha/{ficha}', [FichaController::class, 'update'])->name('update.ficha');
-Route::delete('/ficha/{ficha}', [FichaController::class, 'destroy'])->name('destroy.ficha');
+//Rutas de la Inspecccion **poner esta seccion al final de las rutas**
+
+Route::get('/Inspeccion',            [InpeccioneController::class,  'index'])->name('index.inspeccion');
+Route::get('/Inspeccion/agregar',    [InpeccioneController::class, 'create'])->name('create.inspeccion');
+Route::post('/Inspeccion/guardar',   [InpeccioneController::class, 'store'])->name('store.inspeccion');
