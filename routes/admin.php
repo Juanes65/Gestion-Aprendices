0c198dd049ea801830fe28a90fe07ca3eba9b573
Juanes <juanflorezospina22@gmail.com>
2022-06-30 11:21:39 -0500

    porfavor

--- routes/admin.php
+++ routes/admin.php
@@ -8,4 +8,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 // Rutas de las fichas
 Route::get('/fichas',             [FichaController::class, 'index']) ->name('index.ficha');
 Route::get('/fichas/agregar',     [FichaController::class, 'create'])->name('create.ficha');
-Route::post('/fichas/almacenar',  [FichaController::class, 'store']) ->name('store.ficha');
