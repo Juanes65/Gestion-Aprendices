a963c7eb9a01a0b38aa7c4792e1a94076c5b0fb5
Juanes <juanflorezospina22@gmail.com>
2022-06-30 11:03:16 -0500

    probando

--- routes/admin.php
+++ routes/admin.php
@@ -9,43 +9,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Route::get('/fichas',             [FichaController::class, 'index']) ->name('index.ficha');
-Route::get('/fichas/agregar',     [FichaController::class, 'create'])->name('create.ficha');
-Route::post('/fichas/almacenar',  [FichaController::class, 'store']) ->name('store.ficha');
 Route::get('/ficha/editar/{ficha}', [FichaController::class, 'edit'])->name('edit.ficha');
 Route::put('/ficha/{ficha}', [FichaController::class, 'update'])->name('update.ficha');
 Route::delete('/ficha/{ficha}', [FichaController::class, 'destroy'])->name('destroy.ficha');
