eed17ecf81a5102efa3a5de53b3a42486a67a77f
Juanes <juanflorezospina22@gmail.com>
2022-06-30 10:40:46 -0500

    solicion error

--- routes/admin.php
+++ routes/admin.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Rutas de las fichas
-Route::get('/ficha', [FichaController::class, 'index'])->name('index.ficha');
-Route::get('/ficha/agregar', [FichaController::class, 'create'])->name('create.ficha');
-Route::post('/ficha/almacenar', [FichaController::class, 'store'])->name('store.ficha');
+
 Route::get('/ficha/editar/{ficha}', [FichaController::class, 'edit'])->name('edit.ficha');
 Route::put('/ficha/{ficha}', [FichaController::class, 'update'])->name('update.ficha');
 Route::delete('/ficha/{ficha}', [FichaController::class, 'destroy'])->name('destroy.ficha');
