14faac9a69be771f6fdea09c617e70ececec2b80
Juanes <juanflorezospina22@gmail.com>
2022-06-30 11:17:16 -0500

    probando

--- routes/admin.php
+++ routes/admin.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Rutas de las fichas
-
-
-
-Route::get('/ficha/editar/{ficha}', [FichaController::class, 'edit'])->name('edit.ficha');
-Route::put('/ficha/{ficha}', [FichaController::class, 'update'])->name('update.ficha');
-Route::delete('/ficha/{ficha}', [FichaController::class, 'destroy'])->name('destroy.ficha');
